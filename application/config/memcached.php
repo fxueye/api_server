2c2c0add78d52ca80406f2e93e1b5ddc8d57bd78
shikewei <281431280@qq.com>
2018-08-12 11:21:05 +0800

    p

--- application/config/memcached.php
+++ application/config/memcached.php
@@ -12,8 +12,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config = array(
 	'default' => array(
-		'hostname' => '127.0.0.1',
-		'port'     => '11211',
+		'hostname' => getenv('MEMCACHE_HOST'),
+		'port'     => getenv('MEMCACHE_PORT'),
 		'weight'   => '1',
 	),
 );
